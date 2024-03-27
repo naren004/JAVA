c6b6fd6a6562142139ba8453283a2cc49e39b12e
Narendhranath <113756607+naren004@users.noreply.github.com>
2024-03-27 18:19:04 +0530

    Add files via upload

---
+++ STRING/String compare.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        String s1="hlo";
+        String s2="hlo";
+        System.out.println(s1.compareTo(s2));
+    }
+}
---
+++ STRING/index checking.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args){
+        String s1="this is an index eg";
+        int index1=s1.indexOf("is");
+        int index2=s1.indexOf("an");
+        int index3=s1.indexOf("i");
+        System.out.println(index1+" "+index2+" "+index3);
+
+
+    }
+}
